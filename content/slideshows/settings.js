5355a1cce8e358350db23c6f5df022db94999c7e
sigged <sderdeyn@gmail.com>
2026-05-08 13:30:08 +0200

    Add logos and slideshow app

---
+++ content/slideshows/settings.js
@@ -0,0 +1,11 @@
+var externalSettings = {
+  "IMAGE_FOLDER": "prog-slideshow",
+  "TOTAL_IMAGES": 73,
+  "SLIDE_DURATION": 5000,
+  "FILL_PAN_MODE": true,
+  "RANDOMIZE_ORDER": true,
+
+  "PRIMARY_EXTENSION": ".jpg",
+  "SECONDARY_EXTENSION": ".png",
+  "FALLBACK_IMAGE": "fallback/prog-logo-light-transparent.svg"
+};
